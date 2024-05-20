6e482f49d8e3dc52d3512f48aeee3cb8b0848e4a
Christopher Villamarín <alexpila.314@gmail.com>
2024-05-20 11:51:35 -0500

    Refactor signal handling in main function
    Simplified signal reception by directly reading from the channel instead of using select with a single case. Enhanced comments for clarity, adhering to Go documentation standards. Added detailed function documentation for init and main, explaining initialization and signal handling processes.

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/indrod-group/get_device_alarms/auth"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
-// init is a function that runs before main and initializes the log and the token
+// init performs initial setup before the main function executes.
+// It initializes logging and loads environment variables from .env files.
+// It also initializes the authenticator for handling API authentication.
 func init() {
 	initLog()
 	err := godotenv.Load(".env", ".env.development")
 	if err != nil {
 		logrus.Fatal("Error loading .env file")
 	}
-
 	authenticator = auth.InitAuthenticator()
-
 }
 
-// app is an instance of the App structure
+// authenticator holds an instance of the Authenticator which manages authentication.
 var authenticator *auth.Authenticator
 
+// main sets up signal handling and starts background goroutines.
+// It listens for OS termination signals to gracefully shut down the application.
 func main() {
-
+	// Initiates token renewal and alarm tracking in separate goroutines.
 	go authenticator.InitiateTokenRenewal()
 	go InitiateTrackingAlarms()
 
-	select {}
+	// Creates a channel to receive operating system signals.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Blocks until a signal is received.
+	<-sigChan
+	// Logs the reception of the signal and exits the program.
+	logrus.Info("Program terminated by interrupt signal")
+	os.Exit(0)
 }
